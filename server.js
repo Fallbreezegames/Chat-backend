5ae568529a93d234530bb023b727760a8c414bf5
Fallbreezegames <fallbreezegames@gmail.com>
2026-02-11 02:02:33 -0800

    Backend upload Commit

---
+++ server.js
@@ -0,0 +1,24 @@
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const PORT = process.env.PORT || 3001; // Render sets PORT automatically
+const httpServer = createServer();
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*', // allows other frontends to connect
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('message', (msg) => {
+    console.log('Received message:', msg);
+    io.emit('message', msg); // broadcast to everyone
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log('Server listening on @{PORT}');
+});
